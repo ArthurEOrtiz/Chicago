a23ea842654034ca13968552e54c3a597712693f
Arthur E Ortiz <100542148+ArthurEOrtiz@users.noreply.github.com>
2024-08-16 08:40:44 -0600

    wrapping up

--- chicago/components/map/marker-components.tsx
+++ chicago/components/map/marker-components.tsx
@@ -17,6 +17,7 @@ export const StationMarkerComponent: React.FC<StationMarkerComponentProps> = ({ 
             title={station.station_descriptive_name}
             position={position}
             onClick={onClick}
+            zIndex={1}
         >
             {children}
         </AdvancedMarker>
@@ -39,12 +40,30 @@ export const TrainMarkerComponent: React.FC<TrainMarkerComponentProps> = ({ trai
 
     const position: google.maps.LatLngLiteral = { lat, lng };
 
+    const trainColor = () => {
+        if (train.rt === 'Red') return 'border-cta-red';
+        if (train.rt === 'Blue') return 'border-cta-blue';
+        if (train.rt === 'G') return 'border-cta-green';
+        if (train.rt === 'Brn') return 'border-cta-brown';
+        if (train.rt === 'P') return 'border-cta-purple';
+        if (train.rt === 'Pexp') return 'border-cta-purple-express';
+        if (train.rt === 'Y') return 'border-cta-yellow';
+        if (train.rt === 'Pink') return 'border-cta-pink';
+        if (train.rt === 'Org') return 'border-cta-orange';
+
+    };
+
     return (
         <AdvancedMarker
             title={train.rt}
             position={position}
+            
         >
-          
+            <div className={`border-2 ${trainColor()} p-1 rounded-lg mb-1`}>
+                <p>{train.destNm}</p>
+                <p>Route: {train.rn}</p>
+            </div>
+     
            
         </AdvancedMarker>
     );
